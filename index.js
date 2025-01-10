b7a5e6a58f940df58e80852cf79c4bd5d023c3b6
Aman123at <ammitiwari1234@gmail.com>
2025-01-10 14:56:51 +0530

    second commit code change

--- index.js
+++ index.js
@@ -5,5 +5,9 @@ app.get("/",(req,res)=>{
     res.json({"message":"HELLO TO CICD Service"})
 })
 
+app.get("/demo",(req,res)=>{
+    res.json({"message":"This is demo endpoint. To check everything is working fine."})
+})
+
 
 app.listen(4000,()=>console.log("Server is running on port : 4000"))
